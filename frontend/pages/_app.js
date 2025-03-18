469586cebb0ed7c83f4a33439c42463aa2172a1b
RUDRAJIT DUTTA <rudrajitdutta9@gmail.com>
2025-03-18 23:33:01 +0530

    Wrap application in AuthProvider and restructure layout for better component organization

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,18 @@
 import "@/styles/globals.css";
 import Navbar from "../components/shared/navbar";
 import Footer from "@/components/shared/footer";
+import { AuthProvider } from '../context/AuthContext'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </>
+    <AuthProvider>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
   )
 }
