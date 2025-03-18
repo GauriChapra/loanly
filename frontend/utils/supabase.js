9bcc5254e9ae5452fb824fa8b117f990fbb2e213
RUDRAJIT DUTTA <rudrajitdutta9@gmail.com>
2025-03-19 01:47:25 +0530

    Refactor Supabase client initialization to check for environment variables and ensure they are defined before creating the client instance.

--- frontend/utils/supabase.js
+++ frontend/utils/supabase.js
@@ -1,6 +1,19 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+let supabase
 
-export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey) 
+if (typeof window !== 'undefined') {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  
+  if (!supabaseUrl || !supabaseAnonKey) {
+    console.error('Supabase URL or Anon Key not defined')
+  }
+  
+  supabase = createBrowserClient(
+    supabaseUrl || '',
+    supabaseAnonKey || ''
+  )
+}
+
+export { supabase }
